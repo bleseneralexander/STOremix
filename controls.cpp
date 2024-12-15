79e862fff07a4b68f5a9e5d36178c3e218895b7e
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2024-12-14 21:18:40 -0600

    Update controls.cpp

--- controls.cpp
+++ controls.cpp
@@ -2,11 +2,17 @@ import "IMP.cpp";
 testnetgpx().sec_options();
 testnetgpx().AddComponent(uicomponent_object);
 testnetgpx().AddComponent(GPU());
+uicomponent_object.GPU;
 testnetgpx().AddComponent("label-brand()");
+uicomponent_object.("label-brand");
 testnetgpx().AddComponent(prod());
+uicomponent_object.prod;
 testnetgpx().AddComponent("artist");
+uicomponent_object.artist;
 testnetgpx().AddComponent(perform());
+uicomponent_object.perform;
 testnetgpx().AddComponent(sec());
+uicomponent_object.sec;
 using job("GPU")
   {
 exec();
